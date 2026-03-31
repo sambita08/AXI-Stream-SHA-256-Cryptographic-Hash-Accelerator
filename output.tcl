056b8a3f68d2e5477c6f3752e69bc761b05f0f0e
Sambita Dutta <162744773+sambita08@users.noreply.github.com>
2026-04-01 03:40:30 +0530

    Create output.tcl

---
+++ output.tcl
@@ -0,0 +1,9 @@
+
+the following output was displayed in the Tcl console -
+
+
+id=00000111   len=     1   sha256=a8100ae6aa1940d0b663bb31cd466142ebbdbd5187131b92d93818987832eb89
+id=00000222   len=     1   sha256=ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb
+id=00000333   len=     3   sha256=ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
+id=00000444   len=    11   sha256=b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9
+$finish called at time : 22350 ns : File "D:/VIVADO_PROJECTS/SHA_256/SHA_256.srcs/sources_1/new/tb_sha.v" Line 146
